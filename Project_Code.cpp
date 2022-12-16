39233ed4d7d527b2d9ff1f36423ab3348d30b174
ronair212 <89920991+ronair212@users.noreply.github.com>
2022-12-15 21:23:20 -0500

    Update and rename EECE7205_Final_Project.cpp to Project_Code.cpp

--- Project_Code.cpp
+++ Project_Code.cpp
@@ -1,10 +1,3 @@
-//
-//  main.cpp
-//  FCE Final Project
-//
-//  Created by Rohit  Nair on 11/26/22.
-//9.1,5.1,3.1,6.1,2.1,1.1,8.1,0.1,7.1,4.1
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
